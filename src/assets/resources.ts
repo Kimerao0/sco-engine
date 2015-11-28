f1b36a4c439186664345ccee52e04eafcf35d419
Kimerao0 <ale.ceruti90@gmail.com>
2015-11-28 20:11:08 +0100

    Update resources.ts
    Description added.

--- src/assets/resources.ts
+++ src/assets/resources.ts
@@ -2,22 +2,22 @@ export const resources: Sco.Model.ResourceTypeAsset[] = [
   {
     name: 'Gold',
     color: '#F7F799',
-    description: '',
+    description: 'Main resource for Technology upgrade, Buildings and Units',
     id: 'GL'
   }, {
     name: 'Iron',
     color: '#736F66',
-    description: '',
+    description: 'Main resource for Buildings and Units',
     id: 'IR'
   }, {
     name: 'Gas',
     color: '#7ABF66',
-    description: '',
+    description: 'Main resource for Units movement',
     id: 'GA'
   }, {
     name: 'Obscure matter',
     color: '#443A37',
-    description: '',
+    description: 'Special Resource for Buildings and Units',
     id: 'MO'
   }
 ];
